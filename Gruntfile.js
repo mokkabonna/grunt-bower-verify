957829dca524fe2840d961882af42786620086eb
Martin Hansen <martin@martinhansen.no>
2013-08-29 22:09:06 +0200

    Removed unused dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,6 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-		mochaTest: {
-			test: {
-				options: {
-					reporter: 'spec'
-				},
-				src: ['test/**/*.js']
-			}
-		},
 		jshint: {
 			tasks: {
 				src: ['tasks/**/*.js'],
@@ -19,7 +11,6 @@ module.exports = function(grunt) {
 			tests: {
 				src: ['test/**/*.js'],
 			}
-
 		},
 		"bower-verify": {
 			test: {
@@ -37,13 +28,10 @@ module.exports = function(grunt) {
 	grunt.loadTasks('tasks');
 
 	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
 	// plugin's task(s), then test the result.
-	grunt.registerTask('test', ['mochaTest']);
 	grunt.registerTask('develop', ['watch:develop']);
-
 };
